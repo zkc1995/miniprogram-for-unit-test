79b4941eeaba0febd7583a0b1793012409695aa2
zhengkaichun <zhengkaichun@ctirobot.com>
2021-03-11 16:13:30 +0800

    [add]添加测试框架及工具

---
+++ README.md
@@ -0,0 +1,4 @@
+### 安装小程序测试工具集及测试框架
+`这里使用jest框架`
+
+`npm install -D miniprogram-simulate jest`
---
+++ components/demo/index.js
@@ -0,0 +1,12 @@
+// components/demo/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    prop: {
+      type: String,
+      value: 'first-demo-for-unit-test'
+    }
+  }
+})
